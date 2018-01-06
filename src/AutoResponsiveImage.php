dd20f4d96be21d80d2993c5ce4da94786eb6492f
Kanyarut R <kanyarut.nice@gmail.com>
2018-01-06 12:16:23 +0700

    init project

---
+++ src/AutoResponsiveImage.php
@@ -0,0 +1,45 @@
+<?php 
+namespace AutoResponsiveImage;
+
+class AutoResponsiveImage
+{
+    $config = array(
+        "quality": 100,
+        "sizes": array(100, 60, 30),
+        "public_root": "",
+        "target_folder": "auto-resizes",
+    );
+    // Return array of images with sizes 
+    public static function resize( $image )
+    {
+        return __FILE__;
+        /*
+        $source = imagecreatefromjpeg($image);
+        list($width, $height) = getimagesize($filename);
+        */
+    }
+
+    // Return HTML code to display responsive image 
+    public static function render( $image, $alt = null )
+    {
+        $result = "<img ";
+
+        $images = AutoResponsiveImage::resize( $image );
+        if( is_array($images) && count($images)>0 )
+        {
+            $result_images = array();
+            foreach( $images as $img )
+            {
+                $result_images[] = $img['file']." ".$img['size']."w";
+            }
+            
+            $result .= "srcset=\"".implode(",",$result_images)."\" ";
+            $result .= "src=\"".$images[0]['file']."\" ";
+        }
+        if( $alt )
+        {
+            $result .= "alt=\"".$alt."\" ";
+        }
+        $result .= "/>";
+    }
+}
